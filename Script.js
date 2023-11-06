bae991e6fce094b4793a271dc6b3086b14498cea
Maycon Douglas Niculau dos Santos <60269933+Maycondo@users.noreply.github.com>
2023-11-06 20:31:05 +0000

    Lupas-Animes

--- Script.js
+++ Script.js
@@ -1,32 +1,49 @@
 // Parte da barra de narvegaçao
 
-const CentroLupa = document.querySelector(".CentroLupa");
-const ModedarkLite = document.querySelector("#Button-Mode");
-const LiteMode = document.querySelector(".Mode-Lite");
+const BarraNave = () => {
+  const CentroLupa = document.querySelector(".CentroLupa");
+  const LiteMode = document.querySelector(".Mode-Lite");
 
-function MOdedarkELite(e) {}
+  // Função para atualizar a posição do círculo com base na posição do mouse
+  function updateCentroLupaPosition(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const centerX = window.innerWidth / 260;
+    const centerY = window.innerHeight / 90;
 
-document.addEventListener("click", MOdedarkELite);
+    // Calcula o ângulo entre o mouse e o centro da janela
+    const angle = Math.atan2(mouseY - centerY, mouseX - centerX);
 
-// Função para atualizar a posição do círculo com base na posição do mouse
-function updateCentroLupaPosition(e) {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  const centerX = window.innerWidth / 260;
-  const centerY = window.innerHeight / 90;
+    // Define o raio do círculo
+    const radius = 8;
 
-  // Calcula o ângulo entre o mouse e o centro da janela
-  const angle = Math.atan2(mouseY - centerY, mouseX - centerX);
+    // Calcula a nova posição do círculo
+    const newX = centerX + radius * Math.cos(angle);
+    const newY = centerY + radius * Math.sin(angle);
 
-  // Define o raio do círculo
-  const radius = 8;
+    // Define a posição do círculo
+    CentroLupa.style.left = newX + "px";
+    CentroLupa.style.top = newY + "px";
+  }
+  document.addEventListener("mousemove", updateCentroLupaPosition);
 
-  // Calcula a nova posição do círculo
-  const newX = centerX + radius * Math.cos(angle);
-  const newY = centerY + radius * Math.sin(angle);
+  const ButtonMode = document.getElementById("Button-Mode");
+  const body = document.body;
 
-  // Define a posição do círculo
-  CentroLupa.style.left = newX + "px";
-  CentroLupa.style.top = newY + "px";
-}
-document.addEventListener("mousemove", updateCentroLupaPosition);
+  function ModeDark() {
+    let IcoModeLight = '<i class="fa-solid fa-sun"></i>';
+    let IconeModeDark = '<i class="fa-solid fa-moon"></i>';
+
+    if (body.classList.toggle("Mode-Dark")) {
+      ButtonMode.innerHTML = IcoModeLight;
+    } else
+      body.classList.toString(
+        "Mode-Linght",
+        (ButtonMode.innerHTML = IconeModeDark)
+      );
+  }
+
+  ButtonMode.addEventListener("click", ModeDark);
+};
+
+BarraNave();
